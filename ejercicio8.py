435295097424ad021841ff56fa5b6dba023887e3
nickisainez <113309085+nickisainez@users.noreply.github.com>
2022-10-19 20:40:38 -0500

    Revert "Add solution to exercise 8"

--- ejercicio8.py
+++
@@ -1,6 +0,0 @@
-name = input(f"Ingrese su nombre: ")
-last_name = input(f"Ingrese su apellido: ")
-age = input(f"Ingrese su edad: ")
-
-person = { "name": name.capitalize(), "last_name": last_name.capitalize(), "age": age}
-print(f"Hola, mi nombre es {person['name']} {person['last_name']} y tengo {person['age']} años.")
